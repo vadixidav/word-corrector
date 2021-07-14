98a14b7beb7064c1b1335873c316c7dbd4559084
Geordon Worley <vadixidav@gmail.com>
2021-07-13 20:51:47 -0500

    finish implementation and use triple_accel (faster than alternatives)

--- examples/correct_dict.rs
+++ examples/correct_dict.rs
@@ -6,53 +6,50 @@ use std::{
     io::{BufRead, BufReader},
     time::Instant,
 };
-use word_corrector::Index;
+use word_corrector::Dictionary;
 
 fn main() {
-    let mut index = Index::new();
+    let mut index = Dictionary::new();
 
     // Open the dictionary of words.
     let file = BufReader::new(File::open("en.txt").expect("unable to open dictionary file"));
     // Add every line to the index.
     let old_time = Instant::now();
-    for (ix, line) in file.lines().enumerate() {
+    for line in file.lines() {
         let line = line.expect("unable to read line");
         index.add_word(line);
-        if ix % 100 == 0 {
-            eprintln!("proccessed index {}", ix);
-        }
     }
     let new_time = Instant::now();
 
     eprintln!(
-        "added {} lines at {} lines per second",
-        index.hgg.len(),
-        index.hgg.len() as f64 / (new_time - old_time).as_secs_f64()
+        "added {} words to index at {} words per second",
+        index.len(),
+        index.len() as f64 / (new_time - old_time).as_secs_f64(),
     );
 
-    // Test the recall.
-    let old_time = Instant::now();
-    let mut correct = 0usize;
-    for ix in 0..index.hgg.len() {
-        let word = index.hgg.get_key(ix).unwrap().0.clone();
-        let typo_word = induce_typo(&word);
-        let found_word = index
-            .correct(&typo_word, 16)
-            .expect("unable to correct word; empty index");
-        if word == found_word {
-            correct += 1;
+    for quality in 1..=4 {
+        // Test the recall.
+        let mut correct = 0usize;
+        let old_time = Instant::now();
+        for ix in 0..index.len() {
+            let word = index.get_word(ix).to_owned();
+            let typo_word = induce_typo(&word);
+            let found_word = index
+                .correct(&typo_word)
+                .expect("unable to correct word; empty index");
+            if word == found_word {
+                correct += 1;
+            }
         }
-        if ix % 1000 == 0 {
-            eprintln!("processed index {}", ix);
-        }
+        let new_time = Instant::now();
+
+        eprintln!(
+            "corrected words with recall {} at {} corrections per second with quality {}",
+            correct as f64 / index.len() as f64,
+            index.len() as f64 / (new_time - old_time).as_secs_f64(),
+            quality
+        );
     }
-    let new_time = Instant::now();
-
-    eprintln!(
-        "recall {} at {} corrections per second",
-        correct as f64 / index.hgg.len() as f64,
-        index.hgg.len() as f64 / (new_time - old_time).as_secs_f64()
-    );
 }
 
 fn induce_typo(word: &str) -> String {
--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,35 @@
 extern crate alloc;
 
 use alloc::{borrow::ToOwned, string::String};
-use edit_distance::edit_distance;
 use hgg::HggLite;
 use space::{Knn, MetricPoint, Neighbor};
 
-pub struct Index {
-    pub hgg: HggLite<Word, ()>,
+pub struct Dictionary {
+    hgg: HggLite<Word, ()>,
+    lookup_quality: usize,
 }
 
-impl Index {
+impl Dictionary {
     pub fn new() -> Self {
         Self {
             hgg: HggLite::new().exclude_all_searched(true),
+            lookup_quality: 8,
+        }
+    }
+
+    /// Default: `8`
+    pub fn lookup_quality(self, lookup_quality: usize) -> Self {
+        Self {
+            lookup_quality,
+            ..self
+        }
+    }
+
+    /// Default: `64`
+    pub fn insert_quality(self, insert_quality: usize) -> Self {
+        Self {
+            hgg: self.hgg.insert_knn(insert_quality),
+            ..self
         }
     }
 
@@ -22,16 +39,28 @@ impl Index {
         self.hgg.insert(Word(word.into()), ());
     }
 
-    pub fn correct(&self, word: &str, quality: usize) -> Option<String> {
+    pub fn correct(&self, word: &str) -> Option<String> {
         self.hgg
-            .knn(&Word(word.to_owned()), quality)
+            .knn(&Word(word.to_owned()), self.lookup_quality)
             .into_iter()
             .next()
             .map(|Neighbor { index, .. }| self.hgg.get_key(index).unwrap().0.clone())
     }
+
+    pub fn get_word(&self, word: usize) -> &str {
+        &self.hgg.get_key(word).unwrap().0
+    }
+
+    pub fn len(&self) -> usize {
+        self.hgg.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.hgg.is_empty()
+    }
 }
 
-impl Default for Index {
+impl Default for Dictionary {
     fn default() -> Self {
         Self::new()
     }
@@ -43,6 +72,6 @@ impl MetricPoint for Word {
     type Metric = u32;
 
     fn distance(&self, other: &Self) -> Self::Metric {
-        edit_distance(&self.0, &other.0) as u32
+        triple_accel::levenshtein_exp(self.0.as_bytes(), other.0.as_bytes())
     }
 }
